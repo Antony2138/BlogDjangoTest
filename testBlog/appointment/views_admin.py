61eb5ab5159692f5c7cd5fe9a8ca2f65579403e1
antony2138 <toxa.zag.dom@mail.ru>
2024-10-21 23:50:08 +0300

    check commit

--- testBlog/appointment/views_admin.py
+++ testBlog/appointment/views_admin.py
@@ -24,6 +24,7 @@ def add_or_update_service(request, service_id=None):
         if form.is_valid():
             form.save()
             if service:
+
                 messages.success(request, "Услуга успешно сохранена")
             else:
                 messages.success(request, "Услуга успешно добавлена")
