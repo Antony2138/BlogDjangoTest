df89bdea538efe51fe90f5aa20aa62e2c48b9412
Antony2138 <toxa.zag.dom@mail.ru>
2024-11-10 17:44:49 +0300

    fix user profile view

--- testBlog/mainUser/views.py
+++ testBlog/mainUser/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 import json
 from .services import handle_user_registration
-from .utils.db_helpers import get_user_appointment_list, convert_appointment_to_json
-from datetime import datetime
+from .utils.db_helpers import get_user_appointment_list
 
 # Create your views here.
 
@@ -16,19 +15,6 @@ def registration(request):
 def profile(request):
     user = request.user
     user_appointments = get_user_appointment_list(user)
-    user_appointments_json = convert_appointment_to_json(request, user_appointments)
 
-    appointments_list = []
-    for appointment in user_appointments_json:
-        appointments_list.append({
-            'client_name': appointment.get('client_name', ''),
-            'client_email': appointment.get('client_email', ''),
-            'client_phone': appointment.get('client_phone', ''),
-            'service_name': appointment.get('service_name', ''),
-            'start_time': datetime.fromisoformat(appointment.get('start_time')),
-            'end_time': datetime.fromisoformat(appointment.get('end_time')),
-
-        })
-    print(appointments_list)
-    context = {'appointments': appointments_list}
+    context = {'appointments': user_appointments}
     return render(request, "mainUser/profile.html", context=context)
