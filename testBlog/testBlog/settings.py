c45e901830b2031e4532407e606ed87f622132c8
antony2138 <toxa.zag.dom@mail.ru>
2024-12-23 21:52:07 +0300

    fix utils

--- testBlog/testBlog/settings.py
+++ testBlog/testBlog/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Где размещается приложение
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['http://localhost']
+CSRF_TRUSTED_ORIGINS = ['http://luckypilka.ru']
 
 # Application definition
 
