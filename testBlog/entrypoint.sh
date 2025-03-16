19a6735ef3424a6b8ecfe07d9d8216b54cd9fd2f
antony2138 <toxa.zag.dom@mail.ru>
2025-03-16 21:59:37 +0300

    rollback without https 2.0

--- testBlog/entrypoint.sh
+++ testBlog/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
-gunicorn testBlog.asgi -b 0.0.0.0:8000 -w 2
+gunicorn testBlog.wsgi -b 0.0.0.0:8000 -w 2
 exec "$@"
