741b71634f0c5fd06d87411925048d18fdab3530
antony2138 <toxa.zag.dom@mail.ru>
2025-03-16 19:44:43 +0300

    https certbot

--- testBlog/mainController/tests.py
+++ testBlog/mainController/tests.py
@@ -1,1 +1,9 @@
 # Create your tests here.
+from django.test import TestCase
+
+
+class TestRandom(TestCase):
+    def test_index(self):
+        # response = self.client.get("/")
+
+        self.assertEqual(200, 200)
